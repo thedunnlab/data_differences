f6d3f2715d2967c7cb98d73c2e9e3a2bfb2b194b
thedunnlab <56451096+thedunnlab@users.noreply.github.com>
2021-11-30 09:33:58 -0500

    Add files via upload

---
+++ 2. SLCMA scripts/02-newDNAm-origCovars-covTest/02-newDNAm-origCovars-covTest-oneadult-20200225.R
@@ -0,0 +1,47 @@
+
+setwd("/data/js95/ALSPAC/ARIES/F7/biopsy2019-rerun/")
+## packages 
+
+## data
+# load("ARIES_betas_noTwin_rfaminst_pcs_20181023.Rdata")
+load("ARIES_BP_rerun_20200225.Rdata")
+
+
+outcome.vec <- colnames(aries)[102:ncol(aries)]
+
+## double check the variable types
+# lapply(aries[,1:101], function(x){
+#   class(x)
+# })
+
+## load function for stage 1 LARS, complete data
+source("LARS-noimpute-function-20200225.R")
+
+adver <- "oneadult"
+
+## a list for the recency vectors 
+recency.vec.list <- list(c(18, 30, 42, 57, 69, 81)/12,
+                         c(8, 21, 33, 61, 85)/12,
+                         c(8, 21, 33, 47, 85)/12,
+                         c(18, 30, 42, 57, 69, 81)/12,
+                         c(21, 33, 61, 85)/12,
+                         c(8, 21, 33, 47, 61, 73)/12,
+                         c(8, 21, 33, 61, 73)/12)
+names(recency.vec.list) <-   c("abuse", "Fscore",  "oneadult", "r_faminst", "nbhqual","parc","mompsy")
+
+
+res <- select.LARS.complete(df = aries, 
+                            outcome.vec = outcome.vec, 
+                            adver = adver,
+                            covars = c(## baseline characteristics,EXCLUDE SES
+                              "SES_parent", "WHITE", "Female", "mom_birthage", "ppregnum", "birth.weight","sustained.smoke",
+                              # cell counts
+                              "Bcell", "CD4T", "CD8T", "Gran", "Mono", "NK"),
+                            hypos <- c("accumulation", "recency"),
+                            exposures <- "default",
+                            recency.vec = recency.vec.list[adver][[1]],
+                            inf.method = "covTest")
+
+save(res, file = paste0("SLCMA-newDNAm-rerun-", adver, "_20200225.Rdata"))
+
+
---
+++ README.txt
@@ -0,0 +1,25 @@
+README
+Documentation for the paper "Updates to data versions and analytic methods influence the reproducibility of results from epigenome-wide association studies"
+Author: 	Alexandre Lussier, PhD
+Last updated:	November 29, 2021
+
+In this folder, you can find the following information regarding our manuscript. 
+
+1. Manuscripts (ADD PDFS). 
+	- Manuscript by Zhu and colleagues, which describes the application of the SLCMA to epigenome-wide settings.
+	- Manuscript by Dunn and colleagues, which describes the application of the SLCMA to analysis the time-dependent relationship between childhood adversity and DNA methylation.
+	- Supplemental
+
+2. SLCMA scripts 
+	- R scripts used to analyze each adversity for each of the main analyses
+	- 01-oldDNAm-origCovars-covTest - old DNAm data, standard covariates, covariance test
+	- 02-newDNAm-origCovars-covTest - new DNAm data, standard covariates, covariance test
+	- 03-newDNAm-origCovars-sI - new DNAm data, standard covariates, selective inference
+	- 04-newDNAm-educFWL-sI - new DNAm data, revised covariates, FWL adjustment, selective inference
+
+
+3. Scripts to analyze the resulting data and generate the figures
+	- 1. DD_overall_differences_2021-09-21.Rmd - general differences between data versions
+	- 2. DD_EWAS_2021-08-20.Rmd - epigenome-wide analysis of childhood adversity or prenatal smoking
+	- 3. DD_SLCMA_process_2021-09-21.Rmd - concatenate results from the SLCMA scripts
+	- 4. DD_figures_2021-08-26.Rmd - final analysis and figures for publication
